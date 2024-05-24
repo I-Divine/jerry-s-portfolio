42a63cea461cdbb1c33231bf4b4c5e482c7df48b
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-05-24 09:38:49 +0100

    color update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      ...colors,
+      night: "#151319",
+      "english-violet": "#494051",
+      thistle: "#ccc3d5",
+      "white-smoke": "#f2f2ef",
+      khaki: "#c4b594",
+      "dutch-white": "#ECDBB7",
+    },
     extend: {},
   },
   plugins: [],
